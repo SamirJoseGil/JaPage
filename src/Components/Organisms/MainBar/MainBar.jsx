b1acb2169b934286b339341e85f5888faf6ce7a3
Samir Jose Giil <144859085+SamirJoseGil@users.noreply.github.com>
2024-08-12 13:18:39 -0500

    Changes navBar links to Upercase

--- src/Components/Organisms/MainBar/MainBar.jsx
+++ src/Components/Organisms/MainBar/MainBar.jsx
@@ -9,16 +9,16 @@ function MainBar() {
             <Container fluid className="d-flex justify-content-between align-items-center">
                 <Navbar.Brand href="/Welcome" className="d-flex align-items-center">
                     <img src="../../../../public/Img/Escudo.png" alt="Escudo" className="navbar-logo" />
-                    <strong>I. E. R. Jaipera</strong>
+                    <strong>INSTITUCION EDUCATIVA RURAL JAIPERA</strong>
                 </Navbar.Brand>
                 <div className="d-flex">
                     <Navbar.Toggle aria-controls="navbarScroll" />
                     <Navbar.Collapse id="navbarScroll">
                         <Nav className="me-auto my-2 my-lg-0" style={{ maxHeight: '100px' }} navbarScroll>
                             <Nav.Link as={Link} to="/Welcome">Inicio</Nav.Link>
-                            <Nav.Link as={Link} to="/Sena">Sena</Nav.Link>
-                            <Nav.Link href="https://sinai.net.co" target="_blank" rel="noopener noreferrer">Sinai</Nav.Link>
-                            <Nav.Link as={Link} to="/Politicas">Politicas</Nav.Link>
+                            <Nav.Link as={Link} to="/Sena">SENA</Nav.Link>
+                            <Nav.Link href="https://sinai.net.co" target="_blank" rel="noopener noreferrer">SINAI</Nav.Link>
+                            <Nav.Link as={Link} to="/Politicas">Normatividad</Nav.Link>
                             <Nav.Link as={Link} to="/Contactanos">Contactanos</Nav.Link>
                             <Nav.Link as={Link} to="/QuienesSomos">Quienes Somos</Nav.Link>
                         </Nav>
