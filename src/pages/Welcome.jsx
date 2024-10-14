16d342923eb3060292af21cc6d56802c3ae9c9e4
Samir Jose Giil <144859085+SamirJoseGil@users.noreply.github.com>
2024-10-14 09:58:34 -0500

    Update img rutes

--- src/pages/Welcome.jsx
+++ src/pages/Welcome.jsx
@@ -4,13 +4,13 @@ import { Container, Nav, Navbar } from 'react-bootstrap';
 
 
 const images = [
-    '../../public/ImgLayout/409090199_893417295913873_6431943884138392477_n.jpg',
-    '../../public/ImgLayout/409103605_894058042516465_3086056766359029981_n.jpg',
-    '../../public/ImgLayout/426383391_884813896774213_8697089442123622479_n.jpg',
-    '../../public/ImgLayout/426400078_887276146527988_6685702721640920374_n.jpg',
-    '../../public/ImgLayout/429510733_893417185913884_8989106216206896317_n.jpg',
-    '../../public/ImgLayout/438299173_935010291754573_2235743285604805511_n.jpg',
-    '../../public/ImgLayout/445221277_947692500486352_1983040342489269207_n.jpg',
+    '/ImgLayout/409090199_893417295913873_6431943884138392477_n.jpg',
+    '/ImgLayout/409103605_894058042516465_3086056766359029981_n.jpg',
+    '/ImgLayout/426383391_884813896774213_8697089442123622479_n.jpg',
+    '/ImgLayout/426400078_887276146527988_6685702721640920374_n.jpg',
+    '/ImgLayout/429510733_893417185913884_8989106216206896317_n.jpg',
+    '/ImgLayout/438299173_935010291754573_2235743285604805511_n.jpg',
+    '/ImgLayout/445221277_947692500486352_1983040342489269207_n.jpg',
 ];
 
 
@@ -22,7 +22,7 @@ function Welcome() {
             <header className="position-relative">
                 <MovingImages />
                 <div className="overlay-image position-absolute d-flex align-items-end image-container">
-                    <img src="../../../../public/Img/Escudo.png" className='title-logo' />
+                    <img src="/Img/Escudo.png" className='title-logo' />
                 </div>
                 <div className="overlay d-flex justify-content-center align-items-end">
                     <div className="title-spacing">
